86b4ba70cb6d8aac81f6db63e6a042597be9a79e
Ayush Dixit <117524157+ayushdixit23@users.noreply.github.com>
2024-01-27 11:09:06 +0530

    8

--- app/redux/slice/apiSlice.js
+++ app/redux/slice/apiSlice.js
@@ -3,9 +3,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const Api = createApi({
 	reducerPath: "Api",
 	baseQuery:
-		fetchBaseQuery({
-			baseUrl: "http://192.168.1.10:7700/api"
-		}),
-	// fetchBaseQuery({ baseUrl: "http://192.168.29.227:7700/api" }),
+		// fetchBaseQuery({
+		// 	baseUrl: "http://192.168.1.10:7700/api"
+		// }),
+		fetchBaseQuery({ baseUrl: "https://work.grovyo.xyz/api" }),
 	endpoints: () => ({}),
 });
