242e9c0a8008d52202c132ee0d105bb01c1342fa
Ayush Dixit <117524157+ayushdixit23@users.noreply.github.com>
2024-02-24 16:19:53 +0530

    57

--- app/main/customization/page.jsx
+++ app/main/customization/page.jsx
@@ -1,11 +1,14 @@
+"use client"
+import { getData } from '@/app/utilsHelper/Useful'
 import React from 'react'
 
 const page = () => {
+	const { id } = getData()
 	return (
 		<>
 			<div className='h-[80vh] text-3xl flex justify-center font-semibold items-center bg-white dark:bg-[#273142]'>
-				Comming Soon...
-			</div>
+				<a target='_blank' href={`http://192.168.29.34:3001/lwozxip?id=${id}`} className='underline'>Customize Your Prosite</a>
+			</div >
 		</>
 	)
 }
